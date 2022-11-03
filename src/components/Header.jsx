f01328e837533cd0bcd3ab7d2ecd4e9fc7d140d2
V3lasco36 <migue16velasco@gmail.com>
2022-11-02 22:07:55 -0400

    primer proyecto con react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+
+    return(
+        <h1 className="text-4xl font-extrabold text-gray-5000 text-center">
+        ¿Cuanto <span className="text-indigo-600"> Dinero </span> Necesitas?
+
+      </h1>
+    )
+}
+
+export default Header;
